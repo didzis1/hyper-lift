7968e77513345100eeef49c7184df0c5bf37c60b
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-09 12:33:36 +0300

    UI Kitten wrapper for the App

--- App.tsx
+++ App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './src/screens/AuthStack';
 import { AuthContextProvider } from './src/contexts/AuthContext';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider } from '@ui-kitten/components';
 
 const App = () => {
   return (
-    <AuthContextProvider>
-      <NavigationContainer>
-        <AuthStack />
-      </NavigationContainer>
-    </AuthContextProvider>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <AuthContextProvider>
+        <NavigationContainer>
+          <AuthStack />
+        </NavigationContainer>
+      </AuthContextProvider>
+    </ApplicationProvider>
   );
 };
 
