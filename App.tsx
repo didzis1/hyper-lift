0dc8a2628d5ed820c659bf1f68f05282d159c26c
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-09 19:00:25 +0300

    Added custom theme to the provider

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './src/screens/AuthStack';
 import { AuthContextProvider } from './src/contexts/AuthContext';
-import * as eva from '@eva-design/eva';
 import { ApplicationProvider } from '@ui-kitten/components';
+import { AuthContext } from './src/contexts/AuthContext';
+import AppTabs from './src/screens/AppTabs';
+
+import * as eva from '@eva-design/eva';
+import { default as theme } from './theme.json';
 
 const App = () => {
+  const { token } = useContext(AuthContext);
+
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
       <AuthContextProvider>
         <NavigationContainer>
-          <AuthStack />
+          {token ? <AppTabs /> : <AuthStack />}
         </NavigationContainer>
       </AuthContextProvider>
     </ApplicationProvider>
