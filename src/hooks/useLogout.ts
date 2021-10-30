a58a1f0054c44f742a891f546f50f1f721280805
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-30 15:28:52 +0300

    Create useLogout.ts

---
+++ src/hooks/useLogout.ts
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import AuthStorageContext from '../contexts/AuthStorageContext';
+import { useApolloClient } from '@apollo/client';
+
+const useLogout = () => {
+  const authStorage = useContext(AuthStorageContext);
+  const apolloClient = useApolloClient();
+
+  const logout = async () => {
+    await authStorage.removeToken();
+    apolloClient.resetStore();
+    console.log('logout finished');
+  };
+
+  return { logout };
+};
+
+export default useLogout;
