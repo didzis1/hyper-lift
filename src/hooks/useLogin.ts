8e00705a3333d6e0d908ebc256db6e6329976fd6
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-30 15:28:50 +0300

    Create useLogin.ts

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,28 @@
+import { useMutation } from '@apollo/client';
+import { useContext } from 'react';
+import AuthStorageContext from '../contexts/AuthStorageContext';
+import { LOGIN } from '../graphql/auth/mutations';
+import { LoginType } from '../types/auth/LoginType';
+import { useApolloClient } from '@apollo/client';
+
+const useLogin = () => {
+  const [mutate, result] = useMutation(LOGIN);
+  const authStorage = useContext(AuthStorageContext);
+  const apolloClient = useApolloClient();
+
+  const login = async (credentials: LoginType) => {
+    const { data } = await mutate({
+      variables: { loginInput: credentials }
+    });
+
+    await authStorage.setToken(data.login.value);
+
+    apolloClient.resetStore();
+
+    return data;
+  };
+
+  return { login, result };
+};
+
+export default useLogin;
