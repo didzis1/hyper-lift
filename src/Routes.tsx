7c25fd0dd267e1d1f345cda2d16d09b0c55c28bb
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-30 15:28:58 +0300

    Update Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,16 +1,21 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './screens/AuthStack';
 import AppTabs from './screens/AppTabs';
-
-import { AuthContext } from './contexts/AuthContext';
+import useCurrentUser from './hooks/useCurrentUser';
+import { ActivityIndicator } from 'react-native';
 
 const Routes = () => {
-  const { token } = useContext(AuthContext);
-  console.log(token);
+  const { currentUser, loading } = useCurrentUser();
+
+  if (loading) {
+    return <ActivityIndicator size='large' />;
+  }
+
+  console.log(currentUser);
   return (
     <NavigationContainer>
-      {token ? <AppTabs /> : <AuthStack />}
+      {currentUser ? <AppTabs /> : <AuthStack />}
     </NavigationContainer>
   );
 };
