854d565e51bc0463c17f46c3762a0d0aa6defad6
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-10-31 10:02:07 +0200

    Update AppTabs.tsx

--- src/screens/AppTabs.tsx
+++ src/screens/AppTabs.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ProfileStack from './ProfileStack';
 
+import { FontAwesome } from '@expo/vector-icons';
+
 const Stack = createBottomTabNavigator();
 
 const AppTabs = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='ProfileStack' component={ProfileStack} />
+      <Stack.Screen
+        name='ProfileStack'
+        component={ProfileStack}
+        options={{
+          title: 'Home',
+          tabBarIcon: () => {
+            return <FontAwesome name='home' size={24} color='black' />;
+          }
+        }}
+      />
     </Stack.Navigator>
   );
 };
