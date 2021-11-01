afd1fd55f3df081ca549f6823eff7a371fe774b7
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-01 23:12:57 +0200

    Moved PaperProvider to Routes

--- App.tsx
+++ App.tsx
@@ -1,27 +1,20 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import Routes from './src/Routes';
 import createApolloClient from './src/utils/createApolloClient';
 import AuthStorage from './src/utils/authStorage';
 import AuthStorageContext from './src/contexts/AuthStorageContext';
-import { Provider as PaperProvider } from 'react-native-paper';
-import ThemeContext, {
-  ThemeContextProvider
-} from './src/contexts/ThemeContext';
+import { ThemeContextProvider } from './src/contexts/ThemeContext';
 
 const authStorage = new AuthStorage();
 const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
-  const { theme } = useContext(ThemeContext);
-
   return (
     <ApolloProvider client={apolloClient}>
       <AuthStorageContext.Provider value={authStorage}>
         <ThemeContextProvider>
-          <PaperProvider theme={theme}>
-            <Routes />
-          </PaperProvider>
+          <Routes />
         </ThemeContextProvider>
       </AuthStorageContext.Provider>
     </ApolloProvider>
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useContext } from 'react';
+import { NavigationContainer, Theme } from '@react-navigation/native';
 import AuthStack from './screens/AuthStack';
 import AppTabs from './screens/AppTabs';
 import useCurrentUser from './hooks/useCurrentUser';
 import { ActivityIndicator } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import ThemeContext from './contexts/ThemeContext';
 
-const Routes = () => {
+import { CombinedDarkTheme, CombinedDefaultTheme } from './theme';
+
+type CombinedThemes = ReactNativePaper.Theme & Theme;
+
+const Routes: React.FC = () => {
   const { currentUser, loading } = useCurrentUser();
+  const { isDarkTheme } = useContext(ThemeContext);
+
+  const theme: CombinedThemes = isDarkTheme
+    ? CombinedDarkTheme
+    : CombinedDefaultTheme;
 
   if (loading) {
     return <ActivityIndicator size='large' />;
@@ -14,9 +25,11 @@ const Routes = () => {
 
   console.log(currentUser);
   return (
-    <NavigationContainer>
-      {currentUser ? <AppTabs /> : <AuthStack />}
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer theme={theme}>
+        {currentUser ? <AppTabs /> : <AuthStack />}
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
