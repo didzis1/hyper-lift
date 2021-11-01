9c9d1996f276d234c88bc65f1f660cd6b13241da
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-01 23:13:00 +0200

    Update ThemeContext.tsx

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,38 +1,27 @@
 import React from 'react';
-import { createContext, useCallback, useMemo, useState } from 'react';
-import { CombinedDarkTheme, CombinedLightTheme } from '../theme';
+import { createContext, useState } from 'react';
 
 const ThemeContext = createContext<{
-  isDarkTheme: boolean;
   toggleTheme: () => void;
-  theme: ReactNativePaper.Theme;
+  isDarkTheme: boolean;
 }>({
-  isDarkTheme: false,
   toggleTheme: () => {},
-  theme: CombinedLightTheme
+  isDarkTheme: false
 });
 
 export const ThemeContextProvider: React.FC = ({ children }) => {
   const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false);
-  let theme = isDarkTheme ? CombinedDarkTheme : CombinedLightTheme;
+  const toggleTheme = () => {
+    return setIsDarkTheme((isDarkTheme) => !isDarkTheme);
+  };
 
-  const toggleTheme = useCallback(() => {
-    return setIsDarkTheme(!isDarkTheme);
-  }, [isDarkTheme]);
-
-  const preferences = useMemo(
-    () => ({
-      toggleTheme,
-      isDarkTheme,
-      theme
-    }),
-    [toggleTheme, isDarkTheme, theme]
-  );
+  const values = {
+    toggleTheme,
+    isDarkTheme
+  };
 
   return (
-    <ThemeContext.Provider value={preferences}>
-      {children}
-    </ThemeContext.Provider>
+    <ThemeContext.Provider value={values}>{children}</ThemeContext.Provider>
   );
 };
 
