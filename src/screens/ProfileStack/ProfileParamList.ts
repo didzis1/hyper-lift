f2713a66366eeb6b6f853a20a786cc1687d752f7
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-01 20:47:38 +0200

    Create ProfileParamList.ts

---
+++ src/screens/ProfileStack/ProfileParamList.ts
@@ -0,0 +1,12 @@
+import { RouteProp } from '@react-navigation/core';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+export type ProfileParamList = {
+  Profile: undefined;
+  MaxLifts: undefined;
+};
+
+export type ProfileNavProps<T extends keyof ProfileParamList> = {
+  navigation: NativeStackNavigationProp<ProfileParamList, T>;
+  route: RouteProp<ProfileParamList, T>;
+};
