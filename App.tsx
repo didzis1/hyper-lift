cdc5b2f8168d33c1b4c6d18fee24b459b2db3321
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-01 20:48:38 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import Routes from './src/Routes';
 import createApolloClient from './src/utils/createApolloClient';
 import AuthStorage from './src/utils/authStorage';
 import AuthStorageContext from './src/contexts/AuthStorageContext';
+import { Provider as PaperProvider } from 'react-native-paper';
+import ThemeContext, {
+  ThemeContextProvider
+} from './src/contexts/ThemeContext';
 
 const authStorage = new AuthStorage();
 const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <ApolloProvider client={apolloClient}>
       <AuthStorageContext.Provider value={authStorage}>
-        <Routes />
+        <ThemeContextProvider>
+          <PaperProvider theme={theme}>
+            <Routes />
+          </PaperProvider>
+        </ThemeContextProvider>
       </AuthStorageContext.Provider>
     </ApolloProvider>
   );
