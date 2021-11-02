c29d435e5e8c6a7dfafa571ce6d3fa5f2ad489a9
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-02 16:04:22 +0200

    Update MaxLifts.tsx

--- src/screens/ProfileStack/MaxLifts.tsx
+++ src/screens/ProfileStack/MaxLifts.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { View } from 'react-native';
 import { ProfileNavProps } from './ProfileParamList';
 import globalStyles from '../../globalStyles';
-import { Text } from 'react-native-paper';
+import { Button, Text } from 'react-native-paper';
 
-const MaxLiftsScreen = ({}: ProfileNavProps<'MaxLifts'>) => {
+const MaxLifts = ({ navigation }: ProfileNavProps<'MaxLifts'>) => {
   return (
     <View style={globalStyles.androidSafeArea}>
-      <Text>Max lifts</Text>
+      <Button mode='contained' onPress={() => navigation.push('AddMaxLift')}>
+        Add Max Lift
+      </Button>
+      <Text>Add Max Lift</Text>
+      <View></View>
     </View>
   );
 };
 
-export default MaxLiftsScreen;
+export default MaxLifts;
