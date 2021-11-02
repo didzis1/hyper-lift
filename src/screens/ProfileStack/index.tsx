61b2e330b3ca4b38515eb50602d064a9ac003ad2
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-02 16:04:18 +0200

    Added new screen

--- src/screens/ProfileStack/index.tsx
+++ src/screens/ProfileStack/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Profile from './Profile';
+import AddMaxLift from './AddMaxLift';
 import MaxLifts from './MaxLifts';
 
 const Stack = createNativeStackNavigator();
@@ -8,8 +9,22 @@ const Stack = createNativeStackNavigator();
 const ProfileStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name='Profile' component={Profile} />
-      <Stack.Screen name='MaxLifts' component={MaxLifts} />
+      <Stack.Screen
+        name='Profile'
+        component={Profile}
+        options={{ title: 'Home', headerTitleAlign: 'center' }}
+      />
+      <Stack.Screen
+        name='MaxLifts'
+        component={MaxLifts}
+        options={{ title: 'Max Lifts', headerTitleAlign: 'center' }}
+      />
+
+      <Stack.Screen
+        name='AddMaxLift'
+        component={AddMaxLift}
+        options={{ title: 'Add new max', headerTitleAlign: 'center' }}
+      />
     </Stack.Navigator>
   );
 };
