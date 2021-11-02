3519a7135b66e8ee23a9d2b531eeb28dc28fdf6e
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-02 16:02:46 +0200

    Added UserType to me resolver hook

--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -1,13 +1,16 @@
 import { useQuery } from '@apollo/client';
 import { ME } from '../graphql/auth/queries';
+import { UserType } from '../types/auth/UserType';
 
 const useCurrentUser = () => {
   const { loading, error, data } = useQuery(ME, {
     fetchPolicy: 'cache-and-network'
   });
 
+  const currentUser: UserType | undefined = data?.me;
+
   return {
-    currentUser: data?.me,
+    currentUser,
     loading,
     error
   };
