aab701e0ee8ae4312ece6b77cad0c35da46244eb
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-02 16:01:50 +0200

    Update globalStyles.js

--- globalStyles.js
+++ globalStyles.js
@@ -1,9 +1,13 @@
 import { StyleSheet } from 'react-native';
+import { useTheme } from 'react-native-paper';
+const theme = useTheme();
 
 export const globalStyles = StyleSheet.create({
   center: {
     flex: 1,
+    width: '100%',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: theme.colors.accent
   }
 });
