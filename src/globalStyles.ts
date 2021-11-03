53ff400c35e6fce9f1bb4f0fe8f3825bce490071
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-03 16:19:55 +0200

    Update globalStyles.ts

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -4,6 +4,25 @@ const globalStyles = StyleSheet.create({
   androidSafeArea: {
     flex: 1,
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  form: {
+    width: '80%'
+  },
+  regularButton: {
+    backgroundColor: '#FE5E41',
+    borderWidth: 2,
+    borderColor: '#FFFFFF',
+    elevation: 0,
+    paddingHorizontal: 15,
+    paddingVertical: 3
+  },
+  caption: {
+    textAlign: 'center'
   }
 });
 
