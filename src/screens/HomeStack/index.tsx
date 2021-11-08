53186a60012192d21a51e2988534a223adb4fb0b
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-08 16:25:59 +0200

    Create index.tsx

---
+++ src/screens/HomeStack/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './Home';
+import MaxLifts from './MaxLifts';
+import Profile from './Profile';
+import CreateRoutine from './CreateRoutine';
+
+const Stack = createNativeStackNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Home'
+        component={Home}
+        options={{ title: 'Home', headerTitleAlign: 'center' }}
+      />
+
+      <Stack.Screen
+        name='Profile'
+        component={Profile}
+        options={{ title: 'Profile', headerTitleAlign: 'center' }}
+      />
+
+      <Stack.Screen
+        name='MaxLifts'
+        component={MaxLifts}
+        options={{ title: 'Max Lifts', headerTitleAlign: 'center' }}
+      />
+
+      <Stack.Screen
+        name='CreateRoutine'
+        component={CreateRoutine}
+        options={{ title: 'Create Routine', headerTitleAlign: 'center' }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default HomeStack;
