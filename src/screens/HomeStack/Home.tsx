776ac40ad252c6eabe41d619c4dd68d127bf5305
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-09 11:41:42 +0200

    Updated information cards

--- src/screens/HomeStack/Home.tsx
+++ src/screens/HomeStack/Home.tsx
@@ -1,14 +1,13 @@
 import React, { useContext } from 'react';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { HomeNavProps } from './HomeParamList';
 import useLogout from '../../hooks/useLogout';
-import { Button, Title, Avatar, Caption, Text } from 'react-native-paper';
-import SafeView from '../../components/SafeView';
+import { Button, Title, Text, FAB, Subheading } from 'react-native-paper';
+
 import ThemeContext from '../../contexts/ThemeContext';
 import useCurrentUser from '../../hooks/useCurrentUser';
-import MaxLiftCard from '../../components/MaxLiftCard';
 import Loading from '../../components/Loading';
-
+import { Ionicons } from '@expo/vector-icons';
 const ProfileScreen = ({ navigation }: HomeNavProps<'Home'>) => {
   const { logout } = useLogout();
   const { currentUser } = useCurrentUser();
@@ -30,76 +29,134 @@ const ProfileScreen = ({ navigation }: HomeNavProps<'Home'>) => {
   };
 
   return (
-    <SafeView>
-      {/* Profile card */}
-      <TouchableOpacity onPress={() => navigation.push('Profile')}>
-        <View style={styles.profileCard}>
-          <Avatar.Image
-            size={80}
-            source={{
-              uri: 'https://avatars.githubusercontent.com/u/53485411?s=400&u=fcf4fe96f8f7654d609be9b62f33675ca4e08990&v=4'
-            }}
-          />
-          <View style={styles.profileTop}>
-            <Title>
-              {currentUser.firstName} {currentUser.lastName}
-            </Title>
-            <Caption>Profile</Caption>
+    <ScrollView>
+      <View style={{ padding: 10 }}>
+        <Title>Today</Title>
+        <Title>Tue 9 Nov</Title>
+        {/* Max lifts card */}
+        <View style={styles.cardContainer}>
+          <View style={styles.leftCardContent}>
+            <View
+              style={[
+                styles.box,
+                styles.shadowProp,
+                { marginBottom: 3, marginRight: 3 }
+              ]}>
+              <Title style={styles.cardText}>Weights lifted</Title>
+              <Subheading style={styles.cardText}>this week</Subheading>
+              <Text style={styles.cardText}>1290 kg</Text>
+            </View>
+            <View
+              style={[
+                styles.box,
+                styles.shadowProp,
+                { marginTop: 3, marginRight: 3 }
+              ]}>
+              <Title style={styles.cardText}>Time spent</Title>
+              <Subheading style={styles.cardText}>in the gym</Subheading>
+              <Text style={styles.cardText}>3h 50min</Text>
+            </View>
+          </View>
+
+          <View
+            style={[
+              styles.rightCardContent,
+              styles.shadowProp,
+              { marginLeft: 3 }
+            ]}>
+            <View>
+              <Title style={styles.cardText}>Max Lifts</Title>
+              {currentUser.maxLifts.map((maxLift) => (
+                <View key={maxLift.id}>
+                  <Text style={styles.cardText}>
+                    {maxLift.exercise} - {maxLift.weight} kg
+                  </Text>
+                </View>
+              ))}
+            </View>
+            <FAB
+              style={styles.fab}
+              small
+              icon='plus'
+              onPress={() => navigation.push('MaxLifts')}
+            />
           </View>
         </View>
-      </TouchableOpacity>
+        {/* Routines card */}
+        <View>
+          <Title>Routines</Title>
+          {currentUser.routines.length === 0 ? (
+            <Text>You currently have no routines</Text>
+          ) : (
+            <Text>Routine found</Text>
+          )}
+          <Button
+            icon={() => (
+              <Ionicons name='ios-add-circle-outline' size={24} color='white' />
+            )}
+            mode='contained'
+            uppercase={false}
+            onPress={() => navigation.push('CreateRoutine')}>
+            Create a new routine
+          </Button>
+        </View>
 
-      {/* Max lifts card */}
-      <View style={styles.card}>
-        <Title style={styles.cardTitle}>Max Lifts</Title>
-
-        <MaxLiftCard maxLifts={currentUser.maxLifts} />
-
-        <Button onPress={() => navigation.push('MaxLifts')}>Max Lifts</Button>
+        <View>
+          <Button onPress={() => handleLogout()}>Log out</Button>
+        </View>
+        <Button onPress={() => toggleTheme()}>Change theme</Button>
       </View>
-
-      {/* Routines card */}
-      <View>
-        <Title>Routines</Title>
-        {currentUser.routines.length === 0 ? (
-          <Text>You currently have no routines</Text>
-        ) : (
-          <Text>Routine found</Text>
-        )}
-        <Button
-          mode='contained'
-          onPress={() => navigation.push('CreateRoutine')}>
-          Create a new routine
-        </Button>
-      </View>
-
-      <View>
-        <Button onPress={() => handleLogout()}>Log out</Button>
-      </View>
-      <Button onPress={() => toggleTheme()}>Change theme</Button>
-    </SafeView>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  card: {
-    width: '80%',
-    height: 200,
-    borderRadius: 16,
-    color: 'white',
-    backgroundColor: '#7E007B',
-    alignSelf: 'center'
+  cardContainer: {
+    flex: 1,
+    padding: 10,
+    height: 250,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row'
   },
-  cardTitle: {
-    color: '#FFFFFF',
-    paddingTop: 5,
-    paddingLeft: 15
+  leftCardContent: {
+    flexDirection: 'column',
+    width: '50%'
   },
-  tableCell: {
+  cardText: {
     color: '#FFFFFF'
   },
+  rightCardContent: {
+    flexDirection: 'row',
+    borderRadius: 20,
+    height: '100%',
+    width: '50%',
+    flexGrow: 1,
+    padding: 8,
+    backgroundColor: '#D3BD60'
+  },
+  box: {
+    flexGrow: 1,
+    borderRadius: 20,
+    padding: 8,
+    backgroundColor: '#81b29a'
+  },
+  shadowProp: {
+    shadowColor: '#000000',
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 5
+  },
+  fab: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    margin: 10,
+    backgroundColor: '#F6F9F8'
+  },
   profileCard: {
-    backgroundColor: '#FFFFFF',
+    backgroundColor: '#D3BD60',
     borderRadius: 15,
     width: '90%',
     flexDirection: 'row',
