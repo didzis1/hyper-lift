64e14e160c91b3fe5b6c6dab855164033ce40420
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-11 16:02:55 +0200

    Update MaxLifts.tsx

--- src/screens/HomeStack/MaxLifts.tsx
+++ src/screens/HomeStack/MaxLifts.tsx
@@ -1,5 +1,5 @@
 import { Formik } from 'formik';
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   StyleSheet,
@@ -9,17 +9,25 @@ import {
   TouchableWithoutFeedback,
   Keyboard
 } from 'react-native';
-import { Button, DataTable, Subheading, Text } from 'react-native-paper';
+import {
+  Button,
+  DataTable,
+  Modal,
+  Portal,
+  Text,
+  Title
+} from 'react-native-paper';
 import FormikTextInput from '../../components/FormikTextInput';
 import { HomeNavProps } from './HomeParamList';
 import useMaxLift from '../../hooks/useMaxLift';
 import { AddMaxLiftType } from '../../types/maxLift/AddMaxLiftType';
 import useCurrentUser from '../../hooks/useCurrentUser';
 import Loading from '../../components/Loading';
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 import globalStyles from '../../globalStyles';
 
 const MaxLifts = ({ navigation }: HomeNavProps<'MaxLifts'>) => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
   const { currentUser } = useCurrentUser();
   const { createMaxLift } = useMaxLift();
 
@@ -41,8 +49,64 @@ const MaxLifts = ({ navigation }: HomeNavProps<'MaxLifts'>) => {
       style={{ flex: 1 }}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <ScrollView>
+          {/* Modal */}
+          <Portal>
+            <Modal
+              visible={modalOpen}
+              dismissable
+              onDismiss={() => setModalOpen(false)}
+              contentContainerStyle={styles.modal}>
+              <Formik
+                initialValues={{ exercise: '', weight: '' }}
+                onSubmit={(values) =>
+                  handleNewMaxLift({
+                    exercise: values.exercise,
+                    weight: Number(values.weight)
+                  })
+                }>
+                {({ handleSubmit }) => (
+                  <View style={globalStyles.form}>
+                    <Ionicons
+                      style={{
+                        alignSelf: 'flex-end',
+                        marginTop: 5,
+                        marginRight: 5
+                      }}
+                      onPress={() => setModalOpen(false)}
+                      name='close-sharp'
+                      size={24}
+                      color='black'
+                    />
+                    <Title>Add new max lift</Title>
+                    <FormikTextInput
+                      name='exercise'
+                      label='Exercise'
+                      placeholder='eg. Bench Press'
+                    />
+
+                    <FormikTextInput
+                      name='weight'
+                      label='Max weight (RM)'
+                      placeholder='100'
+                      keyboardType='number-pad'
+                    />
+
+                    <Button
+                      mode='contained'
+                      dark={true}
+                      uppercase={false}
+                      onPress={handleSubmit}>
+                      Save
+                    </Button>
+                  </View>
+                )}
+              </Formik>
+            </Modal>
+          </Portal>
+          {/* Modal */}
+
           <View style={styles.table}>
-            <DataTable>
+            <DataTable style={{ backgroundColor: '#FFFFFF' }}>
               <DataTable.Header style={styles.listHeader}>
                 <DataTable.Title>
                   <Text style={styles.headerText}>Exercise</Text>
@@ -52,52 +116,27 @@ const MaxLifts = ({ navigation }: HomeNavProps<'MaxLifts'>) => {
                 </DataTable.Title>
               </DataTable.Header>
               {currentUser.maxLifts.map((maxLift) => (
-                <DataTable.Row>
+                <DataTable.Row key={maxLift.id}>
                   <DataTable.Cell>{maxLift.exercise}</DataTable.Cell>
                   <DataTable.Cell numeric>{maxLift.weight} kgs</DataTable.Cell>
-                  <DataTable.Cell numeric>
+                  <DataTable.Cell numeric onPress={() => setModalOpen(true)}>
                     <AntDesign name='edit' size={22} color='#3a443a' />
                   </DataTable.Cell>
                 </DataTable.Row>
               ))}
             </DataTable>
           </View>
-
-          <Subheading style={styles.formHeader}>Add new max lift</Subheading>
-
-          <Formik
-            initialValues={{ exercise: '', weight: '' }}
-            onSubmit={(values) =>
-              handleNewMaxLift({
-                exercise: values.exercise,
-                weight: Number(values.weight)
-              })
-            }>
-            {({ handleSubmit }) => (
-              <View style={globalStyles.form}>
-                <FormikTextInput
-                  name='exercise'
-                  label='Exercise'
-                  placeholder='eg. Bench Press'
-                />
-
-                <FormikTextInput
-                  name='weight'
-                  label='Max weight (RM)'
-                  placeholder='100'
-                  keyboardType='number-pad'
-                />
-
-                <Button
-                  mode='contained'
-                  dark={true}
-                  uppercase={false}
-                  onPress={handleSubmit}>
-                  Save Max Lift
-                </Button>
-              </View>
+          <Button
+            dark={true}
+            icon={() => (
+              <Ionicons name='add-circle-outline' size={24} color='white' />
             )}
-          </Formik>
+            uppercase={false}
+            mode='contained'
+            style={styles.addButton}
+            onPress={() => setModalOpen(true)}>
+            Add
+          </Button>
         </ScrollView>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
@@ -106,17 +145,19 @@ const MaxLifts = ({ navigation }: HomeNavProps<'MaxLifts'>) => {
 
 const styles = StyleSheet.create({
   listHeader: {
-    backgroundColor: '#FE5E41'
+    backgroundColor: '#E0e0aa'
   },
-  headerText: {
-    color: '#FFFFFF'
-  },
+  headerText: {},
   table: {
     marginBottom: 35
   },
-
-  formHeader: {
-    textAlign: 'center'
+  addButton: {
+    alignSelf: 'flex-end',
+    textAlign: 'center',
+    marginRight: 16
+  },
+  modal: {
+    backgroundColor: 'white'
   }
 });
 
