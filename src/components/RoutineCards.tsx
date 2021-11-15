fe6833846f8d5ed0183a168b5ad7def8e81217b1
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-15 19:16:14 +0200

    RoutineCards will render routines in their own cards

---
+++ src/components/RoutineCards.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+const RoutineCards = () => {
+  return (
+    <View>
+      <Text>Routine</Text>
+    </View>
+  );
+};
+
+export default RoutineCards;
