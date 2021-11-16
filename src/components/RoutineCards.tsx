f17875ec2dba6151f27ae529596f0efee8150a0e
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-16 18:09:44 +0200

    No routines box created

--- src/components/RoutineCards.tsx
+++ src/components/RoutineCards.tsx
@@ -1,13 +1,89 @@
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
+import { HomeParamList } from '../screens/HomeStack/HomeParamList';
+import { RoutineType } from '../types/RoutineType';
+import { AntDesign } from '@expo/vector-icons';
 
-const RoutineCards = () => {
+type Props = {
+  routines: RoutineType[];
+  navigation: NativeStackNavigationProp<HomeParamList, 'Home'>;
+};
+
+const RoutineCards: React.FC<Props> = ({ routines, navigation }) => {
+  console.log(routines);
   return (
-    <View>
-      <Text>Routine</Text>
+    <View style={styles.container}>
+      {routines.length > 0 ? (
+        <Text>Length is over one</Text>
+      ) : (
+        <View style={styles.contentContainer}>
+          <View style={styles.innerCardContainer}>
+            <Text style={styles.noLiftsText}>
+              You currently have no routines
+            </Text>
+            <Button
+              mode='contained'
+              uppercase={false}
+              icon={() => (
+                <AntDesign name='retweet' size={24} color='#2C4E5B' />
+              )}
+              style={styles.button}
+              labelStyle={styles.buttonText}
+              onPress={() => navigation.navigate('MaxLifts')}>
+              Create a routine
+            </Button>
+          </View>
+        </View>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#2C4E5B',
+    borderRadius: 15,
+    padding: 15,
+    flex: 1,
+    flexDirection: 'column',
+    margin: 15
+  },
+  shadowProp: {
+    elevation: 4
+  },
+  maxLiftRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center'
+  },
+  addIconContainer: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    margin: 12
+  },
+  contentContainer: {
+    marginVertical: 16
+  },
+  innerCardContainer: {
+    width: '70%',
+    alignSelf: 'center'
+  },
+  noLiftsText: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    textAlign: 'center'
+  },
+  button: {
+    marginTop: 10,
+    backgroundColor: '#E9C46A'
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#000000'
+  }
+});
 
 export default RoutineCards;
