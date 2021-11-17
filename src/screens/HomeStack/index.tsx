ad8eaec747277e2a3a5101fe9556176dae444675
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-17 13:35:57 +0200

    TS type checking + modals and generic screens wrapped as group

--- src/screens/HomeStack/index.tsx
+++ src/screens/HomeStack/index.tsx
@@ -7,57 +7,70 @@ import CreateRoutine from './CreateRoutine';
 import ChangeTheme from './ChangeTheme';
 import Measurements from './Measurements';
 import Profile from './Profile';
+import EditMaxLift from './EditMaxLift';
+import AddExercise from './AddExercise';
+import { HomeParamList } from './HomeParamList';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<HomeParamList>();
 
 type Props = {};
 
 const HomeStack: React.FC<Props> = ({}) => {
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name='Home'
-        component={Home}
-        options={{
-          headerShown: false
-        }}
-      />
+      <Stack.Group>
+        <Stack.Screen
+          name='Home'
+          component={Home}
+          options={{
+            headerShown: false
+          }}
+        />
 
-      <Stack.Screen
-        name='Settings'
-        component={Settings}
-        options={{ title: 'Settings', headerTitleAlign: 'center' }}
-      />
+        <Stack.Screen
+          name='Settings'
+          component={Settings}
+          options={{ title: 'Settings', headerTitleAlign: 'center' }}
+        />
 
-      <Stack.Screen
-        name='Profile'
-        component={Profile}
-        options={{ title: 'Profile', headerTitleAlign: 'center' }}
-      />
+        <Stack.Screen
+          name='Profile'
+          component={Profile}
+          options={{ title: 'Profile', headerTitleAlign: 'center' }}
+        />
 
-      <Stack.Screen
-        name='MaxLifts'
-        component={MaxLifts}
-        options={{ title: 'Max Lifts', headerTitleAlign: 'center' }}
-      />
+        <Stack.Screen
+          name='MaxLifts'
+          component={MaxLifts}
+          options={{
+            title: 'Max Lifts',
+            headerTitleAlign: 'center'
+          }}
+        />
 
-      <Stack.Screen
-        name='CreateRoutine'
-        component={CreateRoutine}
-        options={{ title: 'Create Routine', headerTitleAlign: 'center' }}
-      />
+        <Stack.Screen
+          name='CreateRoutine'
+          component={CreateRoutine}
+          options={{ title: 'Create Routine', headerTitleAlign: 'center' }}
+        />
 
-      <Stack.Screen
-        name='ChangeTheme'
-        component={ChangeTheme}
-        options={{ title: 'Preferences', headerTitleAlign: 'center' }}
-      />
+        <Stack.Screen
+          name='ChangeTheme'
+          component={ChangeTheme}
+          options={{ title: 'Preferences', headerTitleAlign: 'center' }}
+        />
 
-      <Stack.Screen
-        name='Measurements'
-        component={Measurements}
-        options={{ title: 'Preferences', headerTitleAlign: 'center' }}
-      />
+        <Stack.Screen
+          name='Measurements'
+          component={Measurements}
+          options={{ title: 'Preferences', headerTitleAlign: 'center' }}
+        />
+      </Stack.Group>
+      <Stack.Group>
+        <Stack.Screen name='AddExercise' component={AddExercise} />
+        {/* <Stack.Screen name='AddMaxLift' component={AddMaxLift} /> */}
+        <Stack.Screen name='EditMaxLift' component={EditMaxLift} />
+      </Stack.Group>
     </Stack.Navigator>
   );
 };
