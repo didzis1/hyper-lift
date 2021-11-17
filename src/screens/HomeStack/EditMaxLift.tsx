315ebadb34ec15e26a80b2345e9e3bbaf276a552
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-17 17:01:15 +0200

    Screen for editing a max lift

--- src/screens/HomeStack/EditMaxLift.tsx
+++ src/screens/HomeStack/EditMaxLift.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Text } from 'react-native-paper';
+import { HomeNavProps } from './HomeParamList';
 
-const EditMaxLift = () => {
+const EditMaxLift: React.FC<HomeNavProps<'EditMaxLift'>> = ({ route }) => {
+  console.log('route', route.params.maxLift.weight);
   return (
     <View>
       <Text>Edit max lift</Text>
+      <View>
+        <Text>{route.params.maxLift.exercise}</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default EditMaxLift;
