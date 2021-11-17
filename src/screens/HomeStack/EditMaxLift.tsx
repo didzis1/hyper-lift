b85fc34fadf46967512d37096134bb89edf43694
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-17 13:36:05 +0200

    Create EditMaxLift.tsx

---
+++ src/screens/HomeStack/EditMaxLift.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+const EditMaxLift = () => {
+  return (
+    <View>
+      <Text>Edit max lift</Text>
+    </View>
+  );
+};
+
+export default EditMaxLift;
