34a827bcd9ab24be15548f4e8c104105f25f557f
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-17 13:36:03 +0200

    Create AddExercise.tsx

---
+++ src/screens/HomeStack/AddExercise.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+const AddExercise = () => {
+  return (
+    <View>
+      <Text>Add exercise</Text>
+    </View>
+  );
+};
+
+export default AddExercise;
