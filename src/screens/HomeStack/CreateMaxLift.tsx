bb41e37b59530625edebb6ee62063d335d4d4b9b
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-19 13:18:09 +0200

    Moved create max lift to its own file

---
+++ src/screens/HomeStack/CreateMaxLift.tsx
@@ -0,0 +1,81 @@
+import { Formik } from 'formik';
+import React from 'react';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  View,
+  StyleSheet
+} from 'react-native';
+import { Button } from 'react-native-paper';
+import FormikTextInput from '../../components/FormikTextInput';
+import useCreateMaxLift from '../../hooks/useCreateMaxLift';
+import { AddMaxLiftType } from '../../types/maxLift/AddMaxLiftType';
+import { HomeNavProps } from './HomeParamList';
+
+const CreateMaxLift: React.FC<HomeNavProps<'CreateMaxLift'>> = ({
+  navigation
+}) => {
+  const { createMaxLift } = useCreateMaxLift();
+
+  const handleNewMaxLift = async (values: AddMaxLiftType) => {
+    try {
+      await createMaxLift(values);
+      navigation.navigate('MaxLifts', {
+        snackBarError: false,
+        snackBarMessage: 'New max lift created successfully'
+      });
+    } catch (error) {
+      navigation.navigate('MaxLifts', {
+        snackBarError: true,
+        snackBarMessage:
+          'An error occured while trying to create a new max lift'
+      });
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}>
+      <TouchableWithoutFeedback>
+        <View style={styles.container}>
+          <View>
+            <Formik
+              initialValues={{
+                exercise: '',
+                weight: ''
+              }}
+              onSubmit={(values) =>
+                handleNewMaxLift({
+                  exercise: values.exercise,
+                  weight: Number(values.weight)
+                })
+              }>
+              {({ handleSubmit }) => (
+                <View>
+                  <FormikTextInput name='exercise' label='Exercise' />
+                  <FormikTextInput
+                    name='weight'
+                    label='Max weight (RM)'
+                    keyboardType='number-pad'
+                  />
+
+                  <Button mode='contained' onPress={() => handleSubmit()}>
+                    Send
+                  </Button>
+                </View>
+              )}
+            </Formik>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20 }
+});
+
+export default CreateMaxLift;
