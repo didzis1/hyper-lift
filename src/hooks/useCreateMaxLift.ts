ac353bc08793af4ab78bc3c0508fcba43b085462
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-19 13:16:32 +0200

    Replaced resetStore with refetchQueries

--- src/hooks/useCreateMaxLift.ts
+++ src/hooks/useCreateMaxLift.ts
@@ -1,10 +1,12 @@
-import { useApolloClient, useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { ADD_MAX_LIFT } from '../graphql/maxLift/mutations';
+import { GET_MAX_LIFTS } from '../graphql/maxLift/queries';
 import { AddMaxLiftType } from '../types/maxLift/AddMaxLiftType';
 
 const useCreateMaxLift = () => {
-  const [mutate, result] = useMutation(ADD_MAX_LIFT);
-  const apolloClient = useApolloClient();
+  const [mutate, result] = useMutation(ADD_MAX_LIFT, {
+    refetchQueries: [GET_MAX_LIFTS]
+  });
 
   const createMaxLift = async (values: AddMaxLiftType) => {
     const { data } = await mutate({
@@ -15,7 +17,6 @@ const useCreateMaxLift = () => {
         }
       }
     });
-    apolloClient.resetStore();
     return data;
   };
 
