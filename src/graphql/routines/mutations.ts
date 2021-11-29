e250b210d10d3182b48f97a7b28cc18d24f99412
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-29 15:20:49 +0200

    Create routine mutation

---
+++ src/graphql/routines/mutations.ts
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_ROUTINE = gql`
+  mutation Mutation($routineData: NewRoutineInput!) {
+    createRoutine(routineData: $routineData) {
+      id
+      description
+      workouts {
+        name
+        exercises {
+          exerciseName
+          setsData {
+            reps
+            set
+            weight
+          }
+        }
+      }
+    }
+  }
+`;
