337bb02afda1e360cd58d6a4c50d8038ae63936b
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-29 12:38:58 +0200

    Hide form if exercise not selected

--- src/screens/HomeStack/CreateMaxLift.tsx
+++ src/screens/HomeStack/CreateMaxLift.tsx
@@ -8,14 +8,16 @@ import {
   StyleSheet,
   TouchableOpacity
 } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import { Button, Subheading } from 'react-native-paper';
 import FormikTextInput from '../../components/FormikTextInput';
 import useCreateMaxLift from '../../hooks/useCreateMaxLift';
 import { AddMaxLiftInput } from '../../types/MaxLiftType';
 import { HomeNavProps } from './HomeParamList';
+import ExerciseCard from '../../components/ExerciseCard';
 
 const CreateMaxLift: React.FC<HomeNavProps<'CreateMaxLift'>> = ({
-  navigation
+  navigation,
+  route
 }) => {
   const { createMaxLift } = useCreateMaxLift();
 
@@ -34,57 +36,86 @@ const CreateMaxLift: React.FC<HomeNavProps<'CreateMaxLift'>> = ({
       });
     }
   };
+  console.log(route.params.exercise);
+  if (route.params.exercise) {
+    return (
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}>
+        <TouchableWithoutFeedback>
+          <View style={styles.container}>
+            <View>
+              <Formik
+                initialValues={{
+                  exercise: route.params.exercise
+                    ? route.params.exercise.name
+                    : '',
+                  weight: ''
+                }}
+                onSubmit={(values) =>
+                  handleNewMaxLift({
+                    exercise: values.exercise,
+                    weight: Number(values.weight)
+                  })
+                }>
+                {({ handleSubmit }) => (
+                  <View>
+                    <View style={styles.formContainer}>
+                      <Subheading>Exercise</Subheading>
+
+                      <TouchableOpacity
+                        onPress={() =>
+                          navigation.navigate('SearchExercise', {})
+                        }>
+                        <ExerciseCard item={route.params.exercise} />
+                      </TouchableOpacity>
+
+                      <FormikTextInput
+                        name='weight'
+                        label='Max weight (RM)'
+                        keyboardType='number-pad'
+                      />
+                    </View>
+                    <Button
+                      mode='contained'
+                      uppercase={false}
+                      onPress={() => handleSubmit()}>
+                      Save
+                    </Button>
+                  </View>
+                )}
+              </Formik>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    );
+  }
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={{ flex: 1 }}>
-      <TouchableWithoutFeedback>
-        <View style={styles.container}>
-          <View>
-            <Formik
-              initialValues={{
-                exercise: '',
-                weight: ''
-              }}
-              onSubmit={(values) =>
-                handleNewMaxLift({
-                  exercise: values.exercise,
-                  weight: Number(values.weight)
-                })
-              }>
-              {({ handleSubmit }) => (
-                <View>
-                  <FormikTextInput name='exercise' label='Exercise' />
-
-                  <TouchableOpacity
-                    onPress={() => navigation.navigate('SearchExercise')}>
-                    <Text>Search Exercise</Text>
-                  </TouchableOpacity>
-                  <FormikTextInput
-                    name='weight'
-                    label='Max weight (RM)'
-                    keyboardType='number-pad'
-                  />
-
-                  <Button
-                    mode='contained'
-                    uppercase={false}
-                    onPress={() => handleSubmit()}>
-                    Save
-                  </Button>
-                </View>
-              )}
-            </Formik>
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+    <View style={styles.firstSearchContainer}>
+      <Button
+        mode='contained'
+        uppercase={false}
+        onPress={() => navigation.navigate('SearchExercise', {})}
+        style={styles.searchButton}>
+        Search for an exercise
+      </Button>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 20 }
+  container: { flex: 1, flexDirection: 'column', padding: 20 },
+  firstSearchContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  formContainer: {},
+  searchButton: {
+    backgroundColor: '#E9C46A'
+  }
 });
 
 export default CreateMaxLift;
