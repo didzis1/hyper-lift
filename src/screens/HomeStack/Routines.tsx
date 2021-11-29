7fbd410beff1bf890d82ac16d717d0cbdbc1a83a
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-29 15:21:17 +0200

    Use query to get routines

--- src/screens/HomeStack/Routines.tsx
+++ src/screens/HomeStack/Routines.tsx
@@ -3,7 +3,16 @@ import { View } from 'react-native';
 import { Text } from 'react-native-paper';
 import { HomeNavProps } from './HomeParamList';
 
+import useGetRoutines from '../../hooks/useGetRoutines';
+import Loading from '../../components/Loading';
+
 const Routines: React.FC<HomeNavProps<'Routines'>> = ({}) => {
+  const { routines, loading } = useGetRoutines();
+
+  console.log('Routines:', routines);
+
+  if (loading) return <Loading />;
+
   return (
     <View>
       <Text>All routines</Text>
