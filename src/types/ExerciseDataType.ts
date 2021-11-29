a2d50d7c1019acf35fe1ce46a69839b38b46ec8c
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-29 12:38:17 +0200

    Typed data for exercise.json

---
+++ src/types/ExerciseDataType.ts
@@ -0,0 +1,11 @@
+export type ExerciseDataType = {
+  name: string;
+  force: string | null;
+  level: string;
+  mechanic: string | null;
+  equipment: string | null;
+  primaryMuscles: string[];
+  secondaryMuscles: string[];
+  instructions: string[];
+  category: string;
+};
