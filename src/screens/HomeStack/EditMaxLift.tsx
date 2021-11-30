435d077c917fd11fea6f5ecbbe45ef3a9a7360de
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-30 13:23:06 +0200

    Updated typing for MaxLift

--- src/screens/HomeStack/EditMaxLift.tsx
+++ src/screens/HomeStack/EditMaxLift.tsx
@@ -5,14 +5,16 @@ import {
   StyleSheet,
   Platform,
   KeyboardAvoidingView,
-  TouchableWithoutFeedback
+  TouchableWithoutFeedback,
+  ScrollView
 } from 'react-native';
-import { Button, Title } from 'react-native-paper';
+import { Button, Headline, Subheading, Text } from 'react-native-paper';
 import FormikTextInput from '../../components/FormikTextInput';
 import { HomeNavProps } from './HomeParamList';
-import { MaxLiftType } from '../../types/MaxLiftType';
+import { EditMaxLiftInput } from '../../types/MaxLiftType';
 import useEditMaxLift from '../../hooks/useEditMaxLift';
 import useDeleteMaxLift from '../../hooks/useDeleteMaxLift';
+import { formatStringDate } from '../../utils/dateFormat';
 
 const EditMaxLift: React.FC<HomeNavProps<'EditMaxLift'>> = ({
   route,
@@ -21,12 +23,13 @@ const EditMaxLift: React.FC<HomeNavProps<'EditMaxLift'>> = ({
   const { editMaxLift } = useEditMaxLift();
   const { deleteMaxLift } = useDeleteMaxLift();
 
-  const handleUpdateMaxLift = async (values: MaxLiftType) => {
+  console.log(new Date(route.params.maxLift.weightHistory[0].date));
+  const handleUpdateMaxLift = async (values: EditMaxLiftInput) => {
     try {
       await editMaxLift({
         id: route.params.maxLift.id,
-        exercise: values.exercise,
-        weight: values.weight
+        weight: values.weight,
+        date: values.date
       });
       navigation.navigate('MaxLifts', {
         snackBarError: false,
@@ -62,50 +65,82 @@ const EditMaxLift: React.FC<HomeNavProps<'EditMaxLift'>> = ({
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={{ flex: 1 }}>
       <TouchableWithoutFeedback>
-        <View style={styles.container}>
-          <Title>Update the weight</Title>
-          <View>
-            <Formik
-              initialValues={{
-                exercise: route.params.maxLift.exercise,
-                weight: route.params.maxLift.weight.toString()
-              }}
-              onSubmit={(values) =>
-                handleUpdateMaxLift({
-                  id: route.params.maxLift.id,
-                  exercise: values.exercise,
-                  weight: Number(values.weight)
-                })
-              }>
-              {({ handleSubmit }) => (
-                <View>
-                  <FormikTextInput name='exercise' label='Exercise' disabled />
-                  <FormikTextInput
-                    name='weight'
-                    label='Max weight (RM)'
-                    keyboardType='number-pad'
-                  />
+        <ScrollView>
+          <View style={styles.container}>
+            <Headline>Update the weight</Headline>
+            <View>
+              <Formik
+                initialValues={{
+                  exercise: route.params.maxLift.exercise,
+                  weight: route.params.maxLift.weight.toString()
+                }}
+                onSubmit={(values) =>
+                  handleUpdateMaxLift({
+                    id: route.params.maxLift.id,
+                    weight: Number(values.weight),
+                    date: new Date()
+                  })
+                }>
+                {({ handleSubmit }) => (
+                  <View>
+                    <FormikTextInput
+                      name='exercise'
+                      label='Exercise'
+                      disabled
+                    />
+                    <FormikTextInput
+                      name='weight'
+                      label='Max weight (RM)'
+                      keyboardType='number-pad'
+                    />
 
-                  <Button
-                    mode='contained'
-                    uppercase={false}
-                    onPress={handleSubmit}>
-                    Update
-                  </Button>
-                </View>
-              )}
-            </Formik>
+                    <Button
+                      mode='contained'
+                      uppercase={false}
+                      onPress={handleSubmit}>
+                      Update
+                    </Button>
+                  </View>
+                )}
+              </Formik>
+            </View>
+
+            {/* History max lifts */}
+            <View style={styles.historyWeightContainer}>
+              <Headline>History of previous lifts</Headline>
+              <View style={styles.historyHeaderContainer}>
+                <Subheading>Weight</Subheading>
+                <Subheading>Date</Subheading>
+              </View>
+              {route.params.maxLift.weightHistory.map((weightHistory) => {
+                return (
+                  <View style={styles.historyDataContainer}>
+                    <View style={styles.historyTextContainer}>
+                      <Text style={styles.historyDataText}>
+                        {weightHistory.weight} kg
+                      </Text>
+                    </View>
+                    <View style={styles.historyTextContainer}>
+                      <Text style={styles.historyDataText}>
+                        {formatStringDate(weightHistory.date)}
+                      </Text>
+                    </View>
+                  </View>
+                );
+              })}
+            </View>
+
+            <View style={styles.removeButtonContainer}>
+              <Button
+                mode='contained'
+                uppercase={false}
+                style={styles.removeButton}
+                onPress={() => handleDeleteMaxLift()}>
+                Delete - {route.params.maxLift.exercise}
+              </Button>
+            </View>
           </View>
-          <View style={styles.removeButtonContainer}>
-            <Button
-              mode='contained'
-              uppercase={false}
-              style={styles.removeButton}
-              onPress={() => handleDeleteMaxLift()}>
-              Delete
-            </Button>
-          </View>
-        </View>
+        </ScrollView>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
@@ -115,6 +150,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20
+  },
+  historyWeightContainer: {
+    paddingVertical: 15
+  },
+  historyHeaderContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'stretch'
+  },
+  historyDataContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#2C4E5B',
+    paddingVertical: 8,
+    marginVertical: 5,
+    borderRadius: 5
+  },
+  historyTextContainer: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  historyDataText: {
+    color: '#E9C46A'
   },
   removeButtonContainer: {
     flex: 1,
--- src/types/MaxLiftType.ts
+++ src/types/MaxLiftType.ts
@@ -2,9 +2,21 @@ export type MaxLiftType = {
   id: string;
   exercise: string;
   weight: number;
+  weightHistory: WeightHistory[];
+};
+
+type WeightHistory = {
+  weight: number;
+  date: string;
 };
 
 export type AddMaxLiftInput = {
   exercise: string;
   weight: number;
+};
+
+export type EditMaxLiftInput = {
+  id: string;
+  weight: number;
+  date: Date;
 };
