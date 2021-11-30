9183115d4aa60308cf60c36ba073bc3a74c43a12
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-30 13:23:16 +0200

    Update useEditMaxLift.ts

--- src/hooks/useEditMaxLift.ts
+++ src/hooks/useEditMaxLift.ts
@@ -1,20 +1,20 @@
 import { useMutation } from '@apollo/client';
 import { EDIT_MAX_LIFT } from '../graphql/maxLift/mutations';
 import { GET_MAX_LIFTS } from '../graphql/maxLift/queries';
-import { MaxLiftType } from '../types/MaxLiftType';
+import { EditMaxLiftInput } from '../types/MaxLiftType';
 
 const useEditMaxLift = () => {
   const [mutate, result] = useMutation(EDIT_MAX_LIFT, {
     refetchQueries: [GET_MAX_LIFTS]
   });
 
-  const editMaxLift = async (values: MaxLiftType) => {
+  const editMaxLift = async (values: EditMaxLiftInput) => {
     const { data } = await mutate({
       variables: {
         maxLiftData: {
           id: values.id,
-          exercise: values.exercise,
-          weight: values.weight
+          weight: values.weight,
+          date: values.date
         }
       }
     });
