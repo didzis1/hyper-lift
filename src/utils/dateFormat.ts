f9d613433b0f85cf8a07ad126492ae423d2ba942
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-30 13:22:53 +0200

    Format the date for max lifts history

---
+++ src/utils/dateFormat.ts
@@ -0,0 +1,16 @@
+export const getCurrentDate = () => {
+  const date = new Date();
+  const day = date.toLocaleDateString('en-us', { weekday: 'short' });
+  const month = date.toLocaleDateString('en-us', { month: 'short' });
+
+  return `${day}, ${date.getDate()} ${month}`;
+};
+
+export const formatStringDate = (date: string) => {
+  const toDateObj = new Date(date);
+  const day = toDateObj.getDate();
+  const month = toDateObj.toLocaleDateString('en-us', { month: 'short' });
+  const year = toDateObj.getFullYear();
+
+  return `${day}. ${month}, ${year}`;
+};
