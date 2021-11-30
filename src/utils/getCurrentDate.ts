66887298ff5c5e90121c15eaf36bcc365e847159
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-30 13:23:09 +0200

    Delete getCurrentDate.ts

--- src/utils/getCurrentDate.ts
+++
@@ -1,7 +0,0 @@
-export const getCurrentDate = () => {
-  const date = new Date();
-  const day = date.toLocaleDateString('en-us', { weekday: 'short' });
-  const month = date.toLocaleDateString('en-us', { month: 'short' });
-
-  return `${day}, ${date.getDate()} ${month}`;
-};
