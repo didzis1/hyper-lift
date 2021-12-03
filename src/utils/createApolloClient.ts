7020e3cac0020fb62ee7ffc7893f3c1aff325452
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-12-03 12:37:46 +0200

    Update createApolloClient.ts

--- src/utils/createApolloClient.ts
+++ src/utils/createApolloClient.ts
@@ -2,6 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import Constants from 'expo-constants';
 import { AuthStorageType } from './authStorage';
+
 const httpLink = createHttpLink({
   uri: `http://${Constants?.manifest?.extra?.local_ip}:4000/graphql`
 });
