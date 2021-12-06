cda93ace39403a4ebd7b17f6ce7f9e251f3e5a6c
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-12-06 14:44:53 +0200

    Update useCreateMaxLift.ts

--- src/hooks/useCreateMaxLift.ts
+++ src/hooks/useCreateMaxLift.ts
@@ -1,14 +1,14 @@
 import { useMutation } from '@apollo/client';
 import { ADD_MAX_LIFT } from '../graphql/maxLift/mutations';
 import { GET_MAX_LIFTS } from '../graphql/maxLift/queries';
-import { AddMaxLiftType } from '../types/maxLift/AddMaxLiftType';
+import { AddMaxLiftInput } from '../types/MaxLiftType';
 
 const useCreateMaxLift = () => {
   const [mutate, result] = useMutation(ADD_MAX_LIFT, {
     refetchQueries: [GET_MAX_LIFTS]
   });
 
-  const createMaxLift = async (values: AddMaxLiftType) => {
+  const createMaxLift = async (values: AddMaxLiftInput) => {
     const { data } = await mutate({
       variables: {
         maxLiftData: {
