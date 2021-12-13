9e02850ec5f478652da6a51c0e4d46433b9eb6ca
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-12-13 11:31:23 +0200

    Added edit and delete buttons

--- src/screens/HomeStack/Routine.tsx
+++ src/screens/HomeStack/Routine.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { List } from 'react-native-paper';
+import { Button, List } from 'react-native-paper';
 import SnackBar from '../../components/SnackBar';
 import { HomeNavProps } from './HomeParamList';
 
 const Routine: React.FC<HomeNavProps<'Routine'>> = ({ navigation, route }) => {
+  const handleDeleteRoutine = () => {
+    try {
+      console.log(route.params.routine._id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View style={{ flex: 1 }}>
       {route.params.routine.workouts.map((workout, index) => {
@@ -48,6 +56,23 @@ const Routine: React.FC<HomeNavProps<'Routine'>> = ({ navigation, route }) => {
           </List.Section>
         );
       })}
+      <View style={styles.buttonContainer}>
+        <View style={styles.button}>
+          <Button mode='contained' uppercase={false} color='#2A9D8F'>
+            Edit
+          </Button>
+        </View>
+        <View style={styles.button}>
+          <Button
+            onPress={() => handleDeleteRoutine()}
+            mode='contained'
+            uppercase={false}
+            color='#E76F51'
+            labelStyle={{ color: '#FFFFFF' }}>
+            Delete
+          </Button>
+        </View>
+      </View>
       <SnackBar navigation={navigation} route={route} />
     </View>
   );
@@ -66,6 +91,17 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     paddingVertical: 3
+  },
+  buttonContainer: {
+    flex: 1,
+    padding: 15,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  button: {
+    marginVertical: 3,
+    width: 200
   }
 });
 
