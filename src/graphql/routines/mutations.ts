10da136d72944ec342d14ad3009f5e372fd6ce8b
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-12-13 12:48:22 +0200

    Edit and Delete routine mutations added

--- src/graphql/routines/mutations.ts
+++ src/graphql/routines/mutations.ts
@@ -16,3 +16,29 @@ export const CREATE_ROUTINE = gql`
     }
   }
 `;
+
+export const DELETE_ROUTINE = gql`
+  mutation Mutation($routineData: DeleteRoutineInput!) {
+    deleteRoutine(routineData: $routineData)
+  }
+`;
+
+export const EDIT_ROUTINE = gql`
+  mutation EditRoutine($routineData: EditRoutineInput!) {
+    editRoutine(routineData: $routineData) {
+      _id
+      description
+      workouts {
+        name
+        exercises {
+          exerciseName
+          reps
+          sets
+        }
+      }
+      totalSets
+      totalReps
+      totalSplits
+    }
+  }
+`;
