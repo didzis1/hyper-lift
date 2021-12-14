7881ccedda6fdb86c65957492b44c0e8d3912f28
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-12-14 18:52:18 +0200

    Added a card that displays text if no routines are found. Initialize state with first routine.

--- src/screens/WorkoutStack/SelectWorkout.tsx
+++ src/screens/WorkoutStack/SelectWorkout.tsx
@@ -1,22 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
-import { Divider, Text, Title, useTheme } from 'react-native-paper';
+import { Subheading, Title, useTheme } from 'react-native-paper';
 import { Picker } from '@react-native-picker/picker';
 import useGetRoutines from '../../hooks/useGetRoutines';
 import Loading from '../../components/Loading';
 import WorkoutCard from '../../components/WorkoutCard';
+// import { Ionicons } from '@expo/vector-icons';
+import { WorkoutNavProps } from './WorkoutParamList';
 
-const SelectWorkout = ({}) => {
+const SelectWorkout: React.FC<WorkoutNavProps<'SelectWorkout'>> = ({}) => {
+  const { routines, loading } = useGetRoutines();
   const [selectedSplit, setSelectedSplit] = useState<string>();
   const { colors } = useTheme();
-  const { routines, loading } = useGetRoutines();
+
+  useEffect(() => {
+    // Set first routine as the selected one
+    // If not set, workout splits are not generated
+    if (routines && routines.length > 0) {
+      setSelectedSplit(routines[0]._id);
+    }
+  }, []);
 
   if (loading) return <Loading />;
 
   if (!routines)
     return (
-      <View>
-        <Text>You currently have no routines</Text>
+      <View style={styles.container}>
+        <View style={styles.noRoutineContainer}>
+          <Subheading style={styles.noRoutineText}>
+            Create a workout to start track your progress and massive gains!
+          </Subheading>
+          {/* Need to figure out how to navigate to a different STACK */}
+          {/* <Button
+            icon={() => (
+              <Ionicons name='rocket-outline' size={24} color='black' />
+            )}
+            onPress={() =>
+              navigation.navigate('HomeStack', {
+                screen: 'CreateRoutine'
+              })
+            }
+            mode='contained'
+            uppercase={false}
+            color={colors.accent}>
+            Create a program
+          </Button> */}
+        </View>
       </View>
     );
 
@@ -24,9 +53,10 @@ const SelectWorkout = ({}) => {
     <ScrollView>
       <View style={[styles.container, { backgroundColor: colors.background }]}>
         <Title>Select your routine</Title>
-        <View>
+        <View style={styles.pickerContainer}>
           <Picker
-            mode='dropdown'
+            style={styles.picker}
+            mode='dialog'
             selectedValue={selectedSplit}
             onValueChange={(split) => setSelectedSplit(split)}>
             {routines.map((routine) => (
@@ -41,7 +71,6 @@ const SelectWorkout = ({}) => {
 
         <View>
           <Title>Select your workout</Title>
-          <Divider />
         </View>
 
         <View style={styles.workoutsContainer}>
@@ -64,9 +93,33 @@ const SelectWorkout = ({}) => {
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 20 },
+  noRoutineContainer: {
+    alignItems: 'center',
+    backgroundColor: '#2C4E5B',
+    paddingVertical: 15,
+    borderRadius: 15,
+    elevation: 5
+  },
+  noRoutineText: {
+    color: '#E9C46A',
+    fontWeight: 'bold',
+    marginHorizontal: 35,
+    marginVertical: 15,
+    textAlign: 'center'
+  },
   workoutsContainer: {
     flex: 1,
     alignItems: 'center'
+  },
+  pickerContainer: {
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#2C4E5B',
+    marginTop: 8,
+    marginBottom: 20
+  },
+  picker: {
+    paddingVertical: 20
   }
 });
 
