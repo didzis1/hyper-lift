1af8e5d5388d66a920787df62fa7a9117c1a90a1
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2022-01-09 15:16:45 +0200

    Workout can now be saved to the database

--- src/screens/WorkoutStack/ActiveWorkout.tsx
+++ src/screens/WorkoutStack/ActiveWorkout.tsx
@@ -1,16 +1,174 @@
+import { Formik, FieldArray } from 'formik';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Button, Subheading, Title } from 'react-native-paper';
+import FormikTextInput from '../../components/FormikTextInput';
 
+import useCreateHistory from '../../hooks/useCreateHistory';
 import { WorkoutNavProps } from './WorkoutParamList';
 
+import { FontAwesome } from '@expo/vector-icons';
+import { AddHistoryInput } from '../../types/HistoryType';
+
 const ActiveWorkout: React.FC<WorkoutNavProps<'ActiveWorkout'>> = ({
-  route
+  route,
+  navigation
 }) => {
+  const { createNewHistoryLift } = useCreateHistory();
+
+  const initialValues = {
+    routineId: route.params.workout.routineId,
+    notes: '',
+    splitName: route.params.workout.name,
+    exercises: route.params.workout.exercises.map((exercise) => ({
+      exerciseName: exercise.exerciseName,
+      volumeSets: [...Array(exercise.sets).keys()].map((set) => ({
+        set: set + 1,
+        reps: '',
+        weight: ''
+      }))
+    }))
+  };
+
+  const handleCreateNewHistory = async (values: AddHistoryInput) => {
+    console.log(values);
+    try {
+      const newHistory = await createNewHistoryLift(values);
+      console.log(newHistory);
+      navigation.navigate('SelectWorkout');
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+    }
+  };
+
   return (
-    <View>
-      <Text>{route.params.workout.name}</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Title>{route.params.workout.name}</Title>
+        <Formik
+          initialValues={initialValues}
+          enableReinitialize
+          validateOnChange={false}
+          onSubmit={(values) =>
+            handleCreateNewHistory({
+              ...values,
+              exercises: values.exercises.map((exercise) => ({
+                ...exercise,
+                volumeSets: exercise.volumeSets.map((volumeSet) => ({
+                  ...volumeSet,
+                  reps: Number(volumeSet.reps),
+                  weight: Number(volumeSet.weight)
+                }))
+              }))
+            })
+          }>
+          {({ handleSubmit, values }) => {
+            return (
+              <View style={{ flex: 1 }}>
+                <FieldArray name='exercises'>
+                  {() => (
+                    <>
+                      {values.exercises.map((exercise, exerciseIndex) => {
+                        return (
+                          <View style={{ marginVertical: 15 }}>
+                            <View style={styles.exerciseContainer}>
+                              <Subheading>{exercise.exerciseName}</Subheading>
+                              <View style={styles.exerciseHeadings}>
+                                <View style={styles.alignHeadingContent}>
+                                  <Text style={styles.headingText}>Set</Text>
+                                </View>
+                                <View style={styles.alignHeadingContent}>
+                                  <Text style={styles.headingText}>
+                                    Reps achieved
+                                  </Text>
+                                </View>
+                                <View style={styles.alignHeadingContent}>
+                                  <Text style={styles.headingText}>
+                                    Weights used
+                                  </Text>
+                                </View>
+                              </View>
+                            </View>
+
+                            {exercise.volumeSets.map(
+                              (volumeSet, volumeSetIndex) => {
+                                return (
+                                  <View style={styles.exerciseHeadings}>
+                                    <View style={styles.alignHeadingContent}>
+                                      <Text>{volumeSet.set}</Text>
+                                    </View>
+                                    <View style={styles.alignHeadingContent}>
+                                      <FormikTextInput
+                                        name={`exercises[${exerciseIndex}].volumeSets[${volumeSetIndex}].reps`}
+                                        placeholder='2'
+                                        keyboardType='number-pad'
+                                      />
+                                    </View>
+                                    <View style={styles.alignHeadingContent}>
+                                      <FormikTextInput
+                                        name={`exercises[${exerciseIndex}].volumeSets[${volumeSetIndex}].weight`}
+                                        placeholder='90 kg'
+                                        keyboardType='number-pad'
+                                      />
+                                    </View>
+                                  </View>
+                                );
+                              }
+                            )}
+                          </View>
+                        );
+                      })}
+                    </>
+                  )}
+                </FieldArray>
+                <Subheading>Optional notes about workout</Subheading>
+                <FormikTextInput
+                  name='notes'
+                  placeholder='Felt stronger on bench press...'
+                />
+                <View>
+                  <Button
+                    onPress={handleSubmit}
+                    mode='contained'
+                    color='#E9C46A'
+                    uppercase={false}
+                    icon={() => (
+                      <FontAwesome name='check' size={24} color='#2C4E5B' />
+                    )}>
+                    Complete workout
+                  </Button>
+                </View>
+              </View>
+            );
+          }}
+        </Formik>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20 },
+  exerciseHeadings: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingVertical: 5
+  },
+  exerciseContainer: {
+    flex: 1
+  },
+  alignHeadingContent: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  headingText: {
+    fontWeight: 'bold',
+    fontSize: 15
+  }
+});
 
 export default ActiveWorkout;
