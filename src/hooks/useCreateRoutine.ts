03a9a96ac58e041194436bc254a200d9e81a8ba4
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2022-01-11 10:48:22 +0200

    fix: import correct input type

--- src/hooks/useCreateRoutine.ts
+++ src/hooks/useCreateRoutine.ts
@@ -1,14 +1,14 @@
 import { useMutation } from '@apollo/client';
 import { CREATE_ROUTINE } from '../graphql/routines/mutations';
 import { GET_ROUTINES } from '../graphql/routines/queries';
-import { RoutineInputType } from '../types/RoutineType';
+import { CreateRoutineInputType } from '../types/RoutineType';
 
 const useCreateRoutine = () => {
   const [mutate, result] = useMutation(CREATE_ROUTINE, {
     refetchQueries: [GET_ROUTINES]
   });
 
-  const createRoutine = async (values: RoutineInputType) => {
+  const createRoutine = async (values: CreateRoutineInputType) => {
     const { data } = await mutate({
       variables: {
         routineData: values
