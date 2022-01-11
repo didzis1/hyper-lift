0f97f8e1c8081a4baee91da55afe7b863308e925
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2022-01-11 13:55:33 +0200

    Update Profile.tsx

--- src/screens/HomeStack/Profile.tsx
+++ src/screens/HomeStack/Profile.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { Button } from 'react-native-paper';
 import useLogout from '../../hooks/useLogout';
+import { HomeNavProps } from './HomeParamList';
 
-const Profile = () => {
+const Profile: React.FC<HomeNavProps<'Profile'>> = ({}) => {
   const { logout } = useLogout();
   return (
-    <View>
-      <Button onPress={() => logout()}>Log out</Button>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Button
+          mode='contained'
+          uppercase={false}
+          style={styles.logout}
+          onPress={() => logout()}>
+          Log out
+        </Button>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logout: {
+    width: '50%',
+    backgroundColor: '#E76F51',
+    alignSelf: 'center'
+  }
+});
 
 export default Profile;
