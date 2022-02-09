a7956fd61dd49544946753f584fef46d6c677e46
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2022-02-09 15:57:49 +0200

    Utility function that calculates percentage of weight increase

---
+++ src/utils/calculateGrowth.ts
@@ -0,0 +1,17 @@
+import { WeightHistory } from '../types/MaxLiftType';
+
+export const calculateGrowth = (
+  weightHistory: WeightHistory[]
+): number | string => {
+  console.log('weighthistory', weightHistory);
+
+  if (weightHistory && weightHistory.length >= 2) {
+    return (
+      (weightHistory[weightHistory.length - 1].weight /
+        weightHistory[0].weight) *
+      10
+    ).toFixed(2);
+  }
+
+  return 0;
+};
